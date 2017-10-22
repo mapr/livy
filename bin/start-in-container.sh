fcb2e8c3a25ad7d19c50051207504932f183959f
Ivan Dzikovskyi <idzikovskyi@mapr.com>
2017-10-22 16:34:04 +0300

    MSPARK-99 SparkVersion file is not present in /opt/mapr/spark

--- bin/start-in-container.sh
+++ bin/start-in-container.sh
@@ -6,10 +6,25 @@ LIVY_VERSION=$(cat "${MAPR_HOME}/livy/livyversion")
 LIVY_HOME="${MAPR_HOME}/livy/livy-${LIVY_VERSION}"
 
 
+get_spark_home() {
+    local SPARK_HOME=""
+    local SPARK_VERSION=""
+    local spark_home_legacy=""
+    if [ -e "${MAPR_HOME}/spark/sparkversion" ]; then
+        SPARK_VERSION=$(cat "${MAPR_HOME}/spark/sparkversion")
+        SPARK_HOME="${MAPR_HOME}/spark/spark-${SPARK_VERSION}"
+    else
+        # Legacy way to find SPARK_HOME
+        spark_home_legacy=$(find "${MAPR_HOME}/spark/" -maxdepth 1 -name "spark-*" -type d | tail -n1)
+        [ -e "${spark_home_legacy}" ] && SPARK_HOME="${spark_home_legacy}"
+    fi
+    echo "${SPARK_HOME}"
+}
+
+
 # Copy hive-site.xml in Spark and set spark.yarn.dist.files in spark-defaults.conf
-if [ -e "${MAPR_HOME}/spark/sparkversion" ]; then
-    SPARK_VERSION=$(cat "${MAPR_HOME}/spark/sparkversion")
-    SPARK_HOME="${MAPR_HOME}/spark/spark-${SPARK_VERSION}"
+SPARK_HOME=$(get_spark_home)
+if [ -e "${SPARK_HOME}" ]; then
     SPARK_CONF="${SPARK_HOME}/conf/spark-defaults.conf"
     SPARK_HIVE_SITE="${SPARK_HOME}/conf/hive-site.xml"
 
