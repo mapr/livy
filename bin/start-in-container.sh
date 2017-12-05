31aa83f9a2cbd665796b9435120fa457a1e96ca8
Ivan Dzikovskyi <idzikovskyi@mapr.com>
2017-12-05 16:44:18 +0200

    MZEP-63 Explicitly set Livy working directory

--- bin/start-in-container.sh
+++ bin/start-in-container.sh
@@ -4,6 +4,7 @@ MAPR_HOME=${MAPR_HOME:-"/opt/mapr"}
 
 LIVY_VERSION=$(cat "${MAPR_HOME}/livy/livyversion")
 LIVY_HOME="${MAPR_HOME}/livy/livy-${LIVY_VERSION}"
+cd "${LIVY_HOME}"
 
 LIVY_CONF_FILES=(
     "${LIVY_HOME}/conf/livy-client.conf"
