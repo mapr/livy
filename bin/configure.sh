73f2e220bfccaee2f81627be6349fc535562eef9
Ivan Dzikovskyi <idzikovskyi@mapr.com>
2018-02-05 15:35:06 +0200

    Cleanup of configure.sh

--- bin/configure.sh
+++ bin/configure.sh
@@ -33,21 +33,29 @@ if [ $? -ne 0 ] ; then
   exit $RETURN_ERR_MAPR_HOME
 fi
 
+LIVY_VERSION=$(cat "${MAPR_HOME}/livy/livyversion")
+LIVY_HOME="${MAPR_HOME}/livy/livy-${LIVY_VERSION}"
 
-LIVY_HOME="/opt/mapr/livy/livy-0.3.0/"
 WARDEN_LIVY_DEST_CONF="$MAPR_HOME/conf/conf.d/warden.livy.conf"
-WARDEN_LIVY_FILE="$LIVY_HOME/conf.new/warden.livy.conf"
-LIVY_VERSION_FILE="$MAPR_HOME/livy/livyversion"
+WARDEN_LIVY_FILE="$LIVY_HOME/conf/warden.livy.conf"
+
 DAEMON_CONF=${MAPR_HOME}/conf/daemon.conf
-VERSION=0.3.0
+
 MAPR_CONF_DIR=${MAPR_CONF_DIR:-"$MAPR_HOME/conf"}
 
+LIVY_CONF_FILES=(
+  "${LIVY_HOME}/conf/livy-client.conf"
+  "${LIVY_HOME}/conf/livy.conf"
+  "${LIVY_HOME}/conf/livy-env.sh"
+  "${LIVY_HOME}/conf/log4j.properties"
+  "${LIVY_HOME}/conf/spark-blacklist.conf"
+)
 LIVY_CONF_TEMPLATES=(
-    "${LIVY_HOME}/conf/livy-client.conf.template     ${LIVY_HOME}/conf/livy-client.conf"
-    "${LIVY_HOME}/conf/livy.conf.template            ${LIVY_HOME}/conf/livy.conf"
-    "${LIVY_HOME}/conf/livy-env.sh.template          ${LIVY_HOME}/conf/livy-env.sh"
-    "${LIVY_HOME}/conf/log4j.properties.template     ${LIVY_HOME}/conf/log4j.properties"
-    "${LIVY_HOME}/conf/spark-blacklist.conf.template ${LIVY_HOME}/conf/spark-blacklist.conf"
+  "${LIVY_HOME}/conf/livy-client.conf.template"
+  "${LIVY_HOME}/conf/livy.conf.template"
+  "${LIVY_HOME}/conf/livy-env.sh.template"
+  "${LIVY_HOME}/conf/log4j.properties.template"
+  "${LIVY_HOME}/conf/spark-blacklist.conf.template"
 )
 
 
@@ -57,73 +65,66 @@ isSecure=0
 customSecure=0
 
 
-function write_version_file() {
-    if [ -f $LIVY_VERSION_FILE ]; then
-        rm -f $LIVY_VERSION_FILE
+change_permissions() {
+  if [ -f $DAEMON_CONF ]; then
+    MAPR_USER=$( awk -F = '$1 == "mapr.daemon.user" { print $2 }' $DAEMON_CONF)
+    MAPR_GROUP=$( awk -F = '$1 == "mapr.daemon.group" { print $2 }' $DAEMON_CONF)
+
+    if [ ! -z "$MAPR_USER" ]; then
+      chown -R ${MAPR_USER} ${LIVY_HOME}
+      chown ${MAPR_USER} ${MAPR_CONF_DIR}
     fi
-    echo $VERSION > $LIVY_VERSION_FILE
-    chown -R $MAPR_USER:$MAPR_GROUP $LIVY_VERSION_FILE
-}
-
-function change_permissions() {
-    if [ -f $DAEMON_CONF ]; then
-        MAPR_USER=$( awk -F = '$1 == "mapr.daemon.user" { print $2 }' $DAEMON_CONF)
-        MAPR_GROUP=$( awk -F = '$1 == "mapr.daemon.group" { print $2 }' $DAEMON_CONF)
-
-        if [ ! -z "$MAPR_USER" ]; then
-            chown -R ${MAPR_USER} ${LIVY_HOME}
-            chown ${MAPR_USER} ${MAPR_CONF_DIR}
-        fi
 
     if [ ! -z "$MAPR_GROUP" ]; then
-            chgrp -R ${MAPR_GROUP} ${LIVY_HOME}
-            chgrp ${MAPR_GROUP} ${MAPR_CONF_DIR}
-        fi
-        chmod -f u+x ${LIVY_HOME}/bin/*
+      chgrp -R ${MAPR_GROUP} ${LIVY_HOME}
+      chgrp ${MAPR_GROUP} ${MAPR_CONF_DIR}
     fi
+    chmod -f u+x ${LIVY_HOME}/bin/*
+  fi
 }
 
-function setup_warden_conf() {
-    if [ -f $WARDEN_LIVY_DEST_CONF ]; then
-        rm -f $WARDEN_LIVY_DEST_CONF
-    fi
-    cp $WARDEN_LIVY_FILE $WARDEN_LIVY_DEST_CONF
-    chown ${MAPR_USER} ${WARDEN_LIVY_DEST_CONF}
-    chgrp ${MAPR_GROUP} ${WARDEN_LIVY_DEST_CONF}
+setup_warden_conf() {
+  if [ -f $WARDEN_LIVY_DEST_CONF ]; then
+    rm -f $WARDEN_LIVY_DEST_CONF
+  fi
+  cp $WARDEN_LIVY_FILE $WARDEN_LIVY_DEST_CONF
+  chown ${MAPR_USER} ${WARDEN_LIVY_DEST_CONF}
+  chgrp ${MAPR_GROUP} ${WARDEN_LIVY_DEST_CONF}
 }
 
-#
-# Check that port is available
-#
-function check_port(){
-    logInfo "Checking that port $1 is available"
-    PORT="$1"
-    if checkNetworkPortAvailability "$PORT" 2>/dev/null; then  
-         { set +x; } 2>/dev/null
-         logInfo "Port $PORT is available"
-    else
-         { set +x; } 2>/dev/null
-         logErr -both "Port $PORT is busy"
-    fi
+check_port(){
+  logInfo "Checking that port $1 is available"
+  PORT="$1"
+  if checkNetworkPortAvailability "$PORT" 2>/dev/null; then  
+    { set +x; } 2>/dev/null
+    logInfo "Port $PORT is available"
+  else
+    { set +x; } 2>/dev/null
+    logErr -both "Port $PORT is busy"
+  fi
 }
 
-function create_restart_livy(){
-    mkdir -p ${MAPR_CONF_DIR}/restart
-    cat > "${MAPR_CONF_DIR}/restart/livy-0.3.0.restart" <<'EOF'
+create_restart_livy(){
+  mkdir -p ${MAPR_CONF_DIR}/restart
+  cat > "${MAPR_CONF_DIR}/restart/livy-0.3.0.restart" <<'EOF'
 #!/bin/bash
 MAPR_USER=${MAPR_USER:-mapr}
 sudo -u ${MAPR_USER} maprcli node services -action restart -name livy -nodes $(hostname)
 EOF
-    chmod +x "${MAPR_CONF_DIR}/restart/livy-0.3.0.restart"
-    chown -R $MAPR_USER:$MAPR_GROUP "${MAPR_CONF_DIR}/restart/livy-0.3.0.restart"
+  chmod +x "${MAPR_CONF_DIR}/restart/livy-0.3.0.restart"
+  chown -R $MAPR_USER:$MAPR_GROUP "${MAPR_CONF_DIR}/restart/livy-0.3.0.restart"
 }
 
-function setup_livy_conf() {
-    local config_template=$1
-    local config_file=$2
-    if [ ! -e "${config_file}" ] && [ -e "${config_template}" ]; then
-        cp "${config_template}" "${config_file}"
+livy_init_confs() {
+  local i=0
+  while [ "$i" -lt "${#LIVY_CONF_FILES[@]}" ]; do
+    local livy_conf_file="${LIVY_CONF_FILES[$i]}"
+    local livy_conf_template="${LIVY_CONF_TEMPLATES[$i]}"
+    if [ ! -e "${livy_conf_file}" ]; then
+      cp "${livy_conf_template}" "${livy_conf_file}"
     fi
+    i=$(expr "$i" + 1)
+  done
 }
 
 
@@ -166,12 +167,8 @@ while [ ${#} -gt 0 ]; do
 done
 
 
-for config in "${LIVY_CONF_TEMPLATES[@]}"; do
-    setup_livy_conf $config
-done
-
+livy_init_confs
 change_permissions
-write_version_file
 setup_warden_conf
 create_restart_livy
 
